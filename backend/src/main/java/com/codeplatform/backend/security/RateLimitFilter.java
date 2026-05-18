28c0e8248c460bb0748c88ba665bfe5fa48c5419
kushal-pandey <kushalpandey10january@gmail.com>
2026-05-18 17:05:53 +0530

    Implement rate limiting using bucket4j

---
+++ backend/src/main/java/com/codeplatform/backend/security/RateLimitFilter.java
@@ -0,0 +1,52 @@
+package com.codeplatform.backend.security;
+
+import io.github.bucket4j.Bucket;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class RateLimitFilter extends OncePerRequestFilter {
+
+    private final RateLimitingService rateLimitingService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();
+        if (requestURI.startsWith("/api/reviews") || requestURI.startsWith("/api/snippets")) {
+
+            String ip = getClientIP(request);
+            Bucket bucket = rateLimitingService.resolveBucket(ip);
+
+            if (bucket.tryConsume(1)) {
+                filterChain.doFilter(request, response);
+            } else {
+                response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+                response.getWriter().write("Too many requests. Please wait a minute and try again.");
+                return;
+            }
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+
+    private String getClientIP(HttpServletRequest request) {
+        String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader == null || xfHeader.isEmpty()) {
+            return request.getRemoteAddr();
+        }
+        return xfHeader.split(",")[0];
+    }
+}
